6ec1663bf8fcd65e1cf6616350ac2d2f52ad2e90
ibrahimabdullahi5 <ibrahimabdullahibare@DESKTOP-0DEOD38.localdomain>
2024-05-26 23:54:03 -0900

    done looping-code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return thankYouMessages;
+  }
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
